df7096b5ee7b9243c768c9fd086fec1e6dcacd72
Leo Lapillonne <llapillo@student.42.fr>
2016-09-07 12:00:03 +0200

    builtin exit

--- includes/builtins/builtin_exit.h
+++ includes/builtins/builtin_exit.h
@@ -6,12 +6,14 @@
 /*   By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/10 10:02:34 by niccheva          #+#    #+#             */
-/*   Updated: 2016/06/10 10:03:14 by niccheva         ###   ########.fr       */
+/*   Updated: 2016/09/07 11:57:29 by llapillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_EXIT_H
 # define BUILTIN_EXIT_H
+
+# include "general.h"
 
 int		builtin_exit(int argc, const char **argv, char **env);
 
