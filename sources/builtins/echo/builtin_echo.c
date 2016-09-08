92b196878fe43d54fd80a61bcc5159c36f7c8b15
Leo Lapillonne <llapillo@student.42.fr>
2016-09-08 22:35:51 +0200

    builtin echo

--- sources/builtins/echo/builtin_echo.c
+++ sources/builtins/echo/builtin_echo.c
@@ -12,6 +12,43 @@
 
 #include "builtins/builtin_echo.h"
 
-int		builtin_echo(int argc, const char **argv, char **env)
+static void		display_arg(t_command *command, const char *arg)
 {
+	char	*new_arg;
+
+	new_arg = NULL;
+	if (is_active(command, "e") && !is_active(command, "E"))
+		ft_putstr(arg);
+	else
+		ft_putstr(ft_strreplace(arg, "\\", "\\"));
+	if (new_arg)
+		ft_strdel(&new_arg);
+}
+
+int				builtin_echo(int argc, const char **argv, char **env)
+{
+	t_command	*command;
+	int			i;
+
+	(void)argc;
+	(void)env;
+	command = builtin_echo_init();
+	if (parse_command(argv, command))
+	{
+		if (is_active(command, "help"))
+			details_of_command(command);
+		else
+		{
+			i = skip_options(argv);
+			while (argv[i])
+			{
+				display_arg(command, argv[i]);
+				if (argv[++i])
+					ft_putchar(' ');
+			}
+			if (!is_active(command, "n"))
+				ft_putchar('\n');
+		}
+	}
+	return (0);
 }
